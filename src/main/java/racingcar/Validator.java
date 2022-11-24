a1f971a02ffa05bb124784436fd0b551b611c453
zelo0 <minorSunbi@gmail.com>
2022-11-24 23:29:30 +0900

    refactor: 검증 로직 전부 Validator로 이동

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -2,11 +2,19 @@ package racingcar;
 
 public class Validator {
 
-    private void checkIfFiveMoreLetters(String[] names) {
+    public void validateEqualOrLessFiveLetters(String[] names) {
         for (String name : names) {
             if (name.length() > 5) {
                 throw new IllegalArgumentException();
             }
+        }
+    }
+
+    public void validateNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
         }
     }
 }
